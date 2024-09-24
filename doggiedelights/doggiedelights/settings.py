09054e45114bfa95bcb30ba8dd8db041dce03842
Dave Vineis <dave.vineis@gmail.com>
2024-09-24 05:44:01 -0500

    add app

--- doggiedelights/doggiedelights/settings.py
+++ doggiedelights/doggiedelights/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+### Setup Email ####
+
+
+### End Email setup ####
